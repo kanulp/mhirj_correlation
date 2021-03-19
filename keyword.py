af9b5d76e38718c851d93e893d5135328d763372
Karan Gajjar <karangajjar.lp@gmail.com>
2021-03-19 01:54:09 -0400

    first commit

--- keyword.py
+++ keyword.py
@@ -1,5 +1,10 @@
-import csv
-with open('data.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+import pandas as pd
+import json
+
+
+# get only the columns you want from the csv file
+df = pd.read_csv('data.csv', usecols=['mdc_ID', 'aircraftno','ATA_Description','LRU','CAS','ATA_Main','ATA_Sub','Discrepancy','Corrective Action'])
+result = df.to_dict(orient='records')
+print(json.dumps(result, indent = 4))
+
+#print(df.head(5))
