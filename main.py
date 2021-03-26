8791ed58c1097cbd3870db26cc26a95b4f9b8513
Karan Gajjar <karangajjar.lp@gmail.com>
2021-03-26 02:37:40 -0400

    new_keyword_list

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process 
 import re
 
-df = pd.read_csv("corr_data_test.csv", usecols=['mdc_ID', 'aircraftno','ATA_Description','LRU','CAS','ATA_Main','ATA_Sub','Discrepancy','Corrective Action', 'p_id', 'Value'],
+df = pd.read_csv("corr_data_test.csv", usecols=['mdc_ID', 'aircraftno','ATA_Description','LRU','CAS','MDC_MESSAGE','EQ_DESCRIPTION','ATA_Main','ATA_Sub','Discrepancy','Corrective Action', 'p_id', 'Value'],
                  skip_blank_lines=True, na_filter=True, encoding= 'unicode_escape').dropna(how = 'all')
 pd.notnull(df["Value"])  
 df["CAS"].fillna("None", inplace = True)  
@@ -12,6 +12,7 @@ df["Value"].fillna(0, inplace = True)
 df.sort_values(['aircraftno', 'p_id', 'mdc_ID'], ascending=[True, True, True])
 df.loc[(df['ATA_Main'] != '33') & df['Value'] != 0]
 result = df[df['mdc_ID'].isin([28919])]
+result
 
 myfile = "words.txt"
 rake_object = RAKE.Rake(myfile)
@@ -22,20 +23,30 @@ def extract_keyword():
 
 keyword_list = extract_keyword()
 
-def keyword_match(mdc_keyword,keyword_list):
-    for key in mdc_keyword:
+def keyword_match(mdc_message,keyword_list):
+    ret_keyword = []
+    for key in mdc_message:
         list_key = list(key) 
         perc_match = 0
-        ret_keyword =''
         if not bool(re.match('[^\w]', list_key[0])):
             for k in keyword_list:
-                perc_match = fuzz.partial_ratio(k.lower(), list_key[0])
-                if perc_match > 60:
-                    ret_keyword =  k
+                if "###" in k :
+                    split_array = k.split("###")
+                    for spr in split_array :
+                        perc_match = fuzz.partial_ratio(spr.lower(), list_key[0])
+                        if perc_match > 70:
+                            ret_keyword.append(spr)
                     break
-                else:
-                    ret_keyword = ""
-            return ret_keyword
+                else : 
+                    perc_match = fuzz.partial_ratio(k.lower(), list_key[0])
+                    print(perc_match, ' ', k.lower(), ' ', list_key[0])
+                    if perc_match > 70:
+                        ret_keyword.append(k)
+                        break
+        if ret_keyword : 
+            break
+   
+    return ret_keyword
            
 
 def keyword_match_descrepancy(pm_message,my_keyword):
@@ -44,8 +55,9 @@ def keyword_match_descrepancy(pm_message,my_keyword):
         perc_match = 0
         status=0
         if not bool(re.match('[^\w]', list_key[0])):
-            perc_match = fuzz.partial_ratio(my_keyword.lower(), list_key[0])
-            if perc_match > 60 : 
+            for k in my_keyword :
+                perc_match = fuzz.partial_ratio(k.lower(), list_key[0])
+            if perc_match > 70 : 
                 status = 3
                 break
             else : 
@@ -53,25 +65,39 @@ def keyword_match_descrepancy(pm_message,my_keyword):
     return status
             
 for item in result.itertuples():
-    cas = rake_object.run(item[5])
-    descrepancy = rake_object.run(item[10])
-   
-    my_keyword = keyword_match(cas,keyword_list)
-    if my_keyword !=  "" :
-        print('found')
+    cas = rake_object.run(item[7])
+    lru = rake_object.run(item[4])
+    eq_desc = rake_object.run(item[6])
+    mdc_message = rake_object.run(item[5])
+    ## pm
+    descrepancy = rake_object.run(item[12])
+    corrective_action = rake_object.run(item[13])
+
+    mdc_keyword_list = []
+    mdc_keyword_list = keyword_match(cas,keyword_list)
+    mdc_keyword_list.extend(keyword_match(lru,keyword_list))
+    mdc_keyword_list.extend(keyword_match(eq_desc,keyword_list))
+    mdc_keyword_list.extend(keyword_match(mdc_message,keyword_list))
+
+    if mdc_keyword_list:
+        status =  keyword_match_descrepancy(descrepancy,mdc_keyword_list)
+        print('status is ',status)
+        if status ==  1 :
+            status =  keyword_match_descrepancy(corrective_action,mdc_keyword_list)
+        else : 
+            print('match with eq_id')
     else:
-        lru = rake_object.run(item[4])
-        my_keyword =  keyword_match(lru,keyword_list)
-    print('my keyword ',my_keyword)
-    status =  keyword_match_descrepancy(descrepancy,my_keyword)
+        print("not found")
+
+    # status =  keyword_match_descrepancy(descrepancy,my_keyword)
     print('status is ',status)
-    if status ==  1 :
-        corrective_action = rake_object.run(item[11])
-        status =  keyword_match_descrepancy(corrective_action.my_keyword)
-    else : 
-        print('match with eq_id')
+    # if status ==  1 :
+    #     corrective_action = rake_object.run(item[11])
+    #     status =  keyword_match_descrepancy(corrective_action.my_keyword)
+    # else : 
+    #     print('match with eq_id')
     
-    result.insert(loc=8, column='Status', value=status)
+    # result.insert(loc=8, column='Status', value=status)
     
     #df["CAS"].fillna("None", inplace = True)  
 
